a2dae5524e447e2ab820751da2f239d2bb3016f2
DrWhilson <drwhilsonmfi@gmail.com>
2025-07-31 17:34:12 +0700

    simple params

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import json
+import argparse
+import tabulate
+
+parser = argparse.ArgumentParser(description="Генератор отчётов")
+
+parser.add_argument(
+    "--file",
+    required=True,
+    type=str,
+    help="Путь к файлу с данными (обязательно)"
+)
+parser.add_argument(
+    "--report",
+    type=str,
+    default="average",
+    help="Отчёт по полю (по умолчанию: average)"
+)
+
+args = parser.parse_args()
+
 
 data = []
 with open('example1.log', 'r', encoding='utf-8') as file:
