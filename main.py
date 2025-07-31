43a888f19e6c9a33530f3812b1ba4181054d90dd
DrWhilson <drwhilsonmfi@gmail.com>
2025-08-01 02:05:24 +0700

    Some reform

--- main.py
+++ main.py
@@ -27,6 +27,23 @@ def load_mult_log(files):
     return data
 
 
+def calc_reprt(data, search_param):
+    group_data = defaultdict(lambda: {"sum": 0, "count": 0})
+
+    for item in data:
+        urlClass = item["url"]
+        vag = item["response_time"]
+        group_data[urlClass]["sum"] += vag
+        group_data[urlClass]["count"] += 1
+
+    result = [
+        {"url": url, "average_vag": data["sum"] / data["count"], "count": data["count"]}
+        for url, data in group_data.items()
+    ]
+
+    return result
+
+
 if __name__ == "__main__":
     # Параметры
     parser = argparse.ArgumentParser(description="Генератор отчётов")
@@ -51,23 +68,12 @@ if __name__ == "__main__":
     log = load_mult_log(args.file)
 
     # Обработка данных
-    group_data = defaultdict(lambda: {"sum": 0, "count": 0})
-
-    for item in log:
-        urlClass = item["url"]
-        vag = item["response_time"]
-        group_data[urlClass]["sum"] += vag
-        group_data[urlClass]["count"] += 1
-
-    result = [
-        {"url": url, "average_vag": data["sum"] / data["count"], "count": data["count"]}
-        for url, data in group_data.items()
-    ]
+    report = calc_reprt(log, args.report)
 
     # Вывод отчёта
     header = ["", "hander", "total", "average_vag"]
     body = []
-    for index, urlClass in enumerate(result):
+    for index, urlClass in enumerate(report):
         row = [index]
         row.append(urlClass["url"])
         row.append(urlClass["count"])
